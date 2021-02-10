537da6113379a73d966f14a5bdd893ce1f89223c
hiroyaonoe <onoehiroya@gmail.com>
2021-02-10 11:20:49 +0000

    Revert "Merge branch 'main' into no-auth"
    This reverts commit 3b54427cd508b81daf1d581fcf2d63e2e80f1c49, reversing
    changes made to 6f02082582f678542086334ec4dc93120bcb4873.

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,5 @@ module RailsTodoapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
